4b16e5e91dceabc1d7eb36a2ec72eb6895da033f
William Moolman <moolman.william@gmail.com>
2021-05-08 16:27:30 +0100

    add kitty config

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ cp -r ~/.config/dunst .config
 cp -r ~/.config/i3 .config
 cp -r ~/.config/i3status .config
 cp -r ~/.config/rofi .config
+cp -r ~/.config/kitty .config
 
 cp -r ~/.scripts .
 cp -r ~/.bin .

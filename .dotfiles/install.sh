7bb87c89aa9125e1a758448ff0308a517f2555e7
William Moolman <moolman.william@gmail.com>
2023-10-29 14:40:35 +0000

    feat(meta): add backup script

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,3 +1,15 @@
 git clone --bare git@github.com:WilliamMoolman/dotfiles.git $HOME/.dotfiles
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config status.showUntrackedFiles no
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no

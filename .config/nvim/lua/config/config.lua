5f3e38bd655400cc5f6a12250ae4561133dd4663
William Moolman <moolman.william@gmail.com>
2024-02-17 10:54:42 +0000

    feat(nvim): update theme + fmt

--- .config/nvim/lua/config/config.lua
+++ .config/nvim/lua/config/config.lua
@@ -3,169 +3,169 @@ require("config.terminal")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-        },
-        lazy=false,
-        keys = {
-            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-        },
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    -- Autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-        },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    -- LSP
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        lazy = true,
-        config = false,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-        },
-    },
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    -- Greeter
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    },
-    {
-        "goolord/alpha-nvim",
-        config = function()
-            require("alpha").setup(require("alpha.themes.dashboard").config)
-        end,
-    },
-    -- Comments
-    "numToStr/Comment.nvim",
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
-    -- Ergonomics
-    {
-        "stevearc/dressing.nvim",
-        opts = {},
-    },
-    -- 'Shatur/neovim-session-manager',
-    {
-        "stevearc/oil.nvim",
-        opts = {},
-        -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    "lewis6991/gitsigns.nvim",
-    {
-        "nvim-java/nvim-java",
-        dependencies = {
-            "nvim-java/lua-async-await",
-            "nvim-java/nvim-java-core",
-            "nvim-java/nvim-java-test",
-            "nvim-java/nvim-java-dap",
-            "MunifTanjim/nui.nvim",
-            "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap",
-            {
-                "williamboman/mason.nvim",
-                opts = {
-                    registries = {
-                        "github:nvim-java/mason-registry",
-                        "github:mason-org/mason-registry",
-                    },
-                },
-            },
-        },
-    },
-    -- {
-    -- 	"m4xshen/hardtime.nvim",
-    -- 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    -- 	opts = {},
-    -- },
-    -- { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
-    -- "tpope/vim-fugitive",
-    -- {
-    --     "epwalsh/obsidian.nvim",
-    --     version = "*", -- recommended, use latest release instead of latest commit
-    --     lazy = true,
-    --     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    --     event = {
-    --       -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --       -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --       "BufReadPre /mnt/c/Users/moolm/Documents/digital-garden/**.md",
-    --       "BufNewFile /mnt/c/Users/moolm/Documents/digital-garden/**.md",
-    --     },
-    --     dependencies = {
-    --         -- Required.
-    --         "nvim-lua/plenary.nvim",
-    --     },
-    --     opts = {
-    --         workspaces = {
-    --             {
-    --                 name = "personal",
-    --                 path = "/mnt/c/Users/moolm/Documents/digital-garden",
-    --             },
-    --         },
-    --     },
-    -- },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		lazy = false,
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	-- LSP
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		lazy = true,
+		config = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- Greeter
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
+		end,
+	},
+	-- Comments
+	"numToStr/Comment.nvim",
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	-- Ergonomics
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	-- 'Shatur/neovim-session-manager',
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	"lewis6991/gitsigns.nvim",
+	{
+		"nvim-java/nvim-java",
+		dependencies = {
+			"nvim-java/lua-async-await",
+			"nvim-java/nvim-java-core",
+			"nvim-java/nvim-java-test",
+			"nvim-java/nvim-java-dap",
+			"MunifTanjim/nui.nvim",
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					registries = {
+						"github:nvim-java/mason-registry",
+						"github:mason-org/mason-registry",
+					},
+				},
+			},
+		},
+	},
+	-- {
+	-- 	"m4xshen/hardtime.nvim",
+	-- 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	-- 	opts = {},
+	-- },
+	-- { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+	-- "tpope/vim-fugitive",
+	-- {
+	--     "epwalsh/obsidian.nvim",
+	--     version = "*", -- recommended, use latest release instead of latest commit
+	--     lazy = true,
+	--     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	--     event = {
+	--       -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--       -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	--       "BufReadPre /mnt/c/Users/moolm/Documents/digital-garden/**.md",
+	--       "BufNewFile /mnt/c/Users/moolm/Documents/digital-garden/**.md",
+	--     },
+	--     dependencies = {
+	--         -- Required.
+	--         "nvim-lua/plenary.nvim",
+	--     },
+	--     opts = {
+	--         workspaces = {
+	--             {
+	--                 name = "personal",
+	--                 path = "/mnt/c/Users/moolm/Documents/digital-garden",
+	--             },
+	--         },
+	--     },
+	-- },
 })
